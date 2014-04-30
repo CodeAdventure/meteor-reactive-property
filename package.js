b72b2cad1d417b99a8c82f308d516d81dcb9f17e
Dominik Guzei <dominik.guzei@gmail.com>
2014-04-30 12:45:46 +0200

    remove not needed api export

--- package.js
+++ package.js
@@ -7,9 +7,6 @@ Package.on_use(function(api) {
   // declare package dependencies
   api.use('deps', ['client', 'server']);
 
-  // export the API
-  api.export && api.export('ReactiveProperty');
-
   // add files for client and server
   api.add_files(
     ['source/ReactiveProperty.js'],
